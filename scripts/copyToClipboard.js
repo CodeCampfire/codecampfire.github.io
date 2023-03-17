4e4962d8c12d089a8614e9f02a57caf7c5678ba7
CodeCampfire <127631158+CodeCampfire@users.noreply.github.com>
2023-03-18 01:00:50 +0300

    Add files via upload

---
+++ scripts/copyToClipboard.js
@@ -0,0 +1,10 @@
+function copyToClipboard(text) {
+    const el = document.createElement('textarea');
+    el.value = text;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    alert('Ссылка скопирована в буфер обмена!');
+  }
+  
